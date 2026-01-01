b72a4034f1c47445897dedb78e4dfa172d3975af
Paulo Leonardo <78887916+levelslh1213@users.noreply.github.com>
2026-01-01 23:59:25 +0000

    publish-versel-v1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>caraca que fera meu mano</h1>
+    return <h1>Gustavão você é fera! <img src="https://jumpshare.com/s/DRXfgGWDa3c9cpKcfOrT"></img> </h1> 
 }
 
 export default Home;
