fe5af5770fafae03b0dc81ee758642d7fef31be7
Paulo Leonardo <78887916+levelslh1213@users.noreply.github.com>
2026-01-02 20:37:49 +0000

    Adiciona os arquivos do manifesto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>caraca que fera meu mano</h1>
+    return <h1>Meu Pai, amo você! Um dia vou me tornar o homem que você quer que eu seja e superar suas expectativas!
+    </h1>
 }
 
 export default Home;
