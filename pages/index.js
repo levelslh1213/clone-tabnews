bacb0e03cdd89ac4eed1d4c3777ae5e39dcd9a4b
Paulo Leonardo <78887916+levelslh1213@users.noreply.github.com>
2026-01-07 03:02:58 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-    return <h1>Meu Pai, amo você! Um dia vou me tornar o homem que você quer que eu seja e superar suas expectativas! 💪
+  return (
+    <h1>
+      Meu Pai, amo você! Um dia vou me tornar o homem que você quer que eu seja
+      e superar suas expectativas! 💪
     </h1>
+  );
 }
 
 export default Home;
